2df7f51d67f02952da5d358760efbf7eee90f085
Reuben Germain <reuben.germain@soprasteria.com>
2024-06-19 14:29:41 +0100

    added & changed some code to show products to appear by price

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-fetch("https://hplussport.com/api/products?order=name")
+fetch("https://hplussport.com/api/products?order=price")
 .then(function(response) {
     return response.json();
 })
 .then(function(jsonData) {
     for(items in jsonData) {
+        // Display all of the product names
         var productName = jsonData[items].name;
         var products = document.createElement("li");
         products.innerHTML = productName;
         document.body.appendChild(products);
 
+        // Display all of the product images
         var productImage = jsonData[items].image;
         var image = document.createElement("img");
         image.setAttribute("src", productImage);
         document.body.appendChild(image);
+
+        // Display all of the product prices
+        var price = jsonData[items].price;
+        var priceElement = document.createElement("h6");
+        priceElement.innerHTML = price;
+        document.body.appendChild(priceElement);
     }
 });
